824538beae3dabcc5b219f4fc3a171371bed09b3
Jay <31777481+iaevelis@users.noreply.github.com>
2017-09-23 12:41:20 -0700

    Create circles-sketch-1.js

---
+++ circles-sketch-1.js
@@ -0,0 +1,13 @@
+background(255);
+strokeWeight(3);
+colorMode(HSB);
+
+while (true) {
+  var h = random(360)
+  stroke(h, 50, 90);
+  fill(h, 50, 100);
+
+  var r = random(30, 60);
+ellipseMode(CENTER)
+  ellipse(mouseX, mouseY, r, r);
+}
